d6003011b4c5b9407c33ffbadc65b6e57e0675ac
NighatAtta <nighat@clustox.com>
2023-02-08 11:47:22 +0500

    adding reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+      //this.screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
    
    env: {
@@ -12,6 +14,7 @@ module.exports = defineConfig({
     "agentId" : "ae5208ef-040a-493b-aa90-51a7831aa3fe",
     "QAsimulatorEnvId": "7811b5ae-8073-43a5-be6c-47049ce8eee4",
     "ProjectId" : "quantified-p1"
-   }
-  }
+   },
+},
 });
+
