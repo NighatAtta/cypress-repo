481403a2966825fbe76687bcc58b967680c168f4
NighatAtta <nighat@clustox.com>
2023-02-13 14:47:44 +0500

    nothing

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
    env: {
     "simulationAPIURL": "https://dialogservice-qa.quantified.ai/",
     "agentId" : "ae5208ef-040a-493b-aa90-51a7831aa3fe",
-    "QAsimulatorEnvId": "7811b5ae-8073-43a5-be6c-47049ce8eee",
+    "QAsimulatorEnvId": "7811b5ae-8073-43a5-be6c-47049ce8eee4",
     "ProjectId" : "quantified-p1"
    },
 },
